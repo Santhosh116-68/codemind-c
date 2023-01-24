9d92ef9e1231ad94bd4c181a74711e868c97e3be
Santhosh116-68 <119840312+Santhosh116-68@users.noreply.github.com>
2023-01-24 22:08:06 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=0;i<n;i++) {         for(j=0;j<n;j++) {             if(i==j) {                 printf("x");             }else if(j==n-1-i) {                 printf("x");             }else {                 printf("0");             }         }         printf(" ");     } }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            if(i==j) {
+                printf("x");
+            }else if(j==n-1-i) {
+                printf("x");
+            }else {
+                printf("0");
+            }
+        }
+        printf("
+");
+    }
+}
